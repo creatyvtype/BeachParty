d543fe83aa57abac4f5c679aada8781554accd9f
Majd Murad <murad.majd@gmail.com>
2016-12-03 22:42:29 -0800

    4 Sizes for beach balls. Nested styling not working for some reason

--- app/js/containers/beach-party.jsx
+++ app/js/containers/beach-party.jsx
@@ -8,7 +8,10 @@ export default class BeachParty extends React.Component {
     render() {
         return (
             <div className="beach-party-page">
+                <BeachBall size="x-large" />
+                <BeachBall size="large" />
                 <BeachBall />
+                <BeachBall size="small" />
             </div>
         )
     }
--- app/js/react_components/beach-ball.jsx
+++ app/js/react_components/beach-ball.jsx
@@ -1,12 +1,33 @@
-import React from 'react'
+import React, { PropTypes } from 'react'
 
 import 'styles/beach-ball'
 
 export default class BeachBall extends React.Component {
     render() {
+        var className = "beach-ball"
+        if (this.props.size) {
+            switch (this.props.size) {
+                case "small":
+                    className += " small"
+                    break
+                case "large":
+                    className += " large"
+                    break
+                case "x-large":
+                    className += " x-large"
+                    break
+                default:
+                    break
+            }
+        }
+        console.log(className)
         return (
-            <div className="beach-ball">
+            <div className={className}>
             </div>
         )
     }
+}
+
+BeachBall.propTypes = {
+    size: React.PropTypes.string
 }
